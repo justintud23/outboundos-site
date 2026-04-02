cb30551e8c1727a35beda5f832e1f19ad58ba246
justintud23 <justin.tudhope@gmail.com>
2026-04-02 15:18:53 -0500

    feat: add /replies page

---
+++ src/app/(dashboard)/replies/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { Header } from '@/components/layout/header'
+import { RepliesClient } from './replies-client'
+import { getReplies } from '@/features/replies/server/get-replies'
+import { resolveOrganization } from '@/lib/auth/resolve-organization'
+
+export default async function RepliesPage() {
+  const { orgId } = await auth()
+
+  if (!orgId) {
+    redirect('/dashboard')
+  }
+
+  const org = await resolveOrganization(orgId)
+  const { replies, total } = await getReplies({ organizationId: org.id })
+
+  return (
+    <>
+      <Header title="Replies" />
+      <div className="flex-1 p-6">
+        <RepliesClient initialReplies={replies} initialTotal={total} />
+      </div>
+    </>
+  )
+}
